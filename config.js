837e59b3f2f0ca6faa44a5bf90ba6441ec65c63d
JoelYepez21 <164281952+JoelYepez21@users.noreply.github.com>
2024-08-15 10:16:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const PAGE_URL = process.env.NODE_ENV === 'production' ? 'palceholder' : 'http://localhost:';
+const PAGE_URL = process.env.NODE_ENV === 'production' ? 'https://backendpaginadb21.onrender.com' : 'http://localhost:';
 
 const MONGO_URI =
   process.env.NODE_ENV === 'production' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_TEST;
